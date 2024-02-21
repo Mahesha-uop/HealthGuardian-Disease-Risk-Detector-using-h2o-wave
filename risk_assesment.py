120f1bff7d0a340816391321232208be96d6e77c
mahesha-uop <maheshaviduranga@gmail.com>
2024-02-21 05:31:41 +0530

    Correct diabetes

--- risk_assesment.py
+++ risk_assesment.py
@@ -39,95 +39,178 @@ predictor = Predict_Diabetes()
 # Main page
 @app('/health_guardian')
 async def serve(q: Q):
-    # Welcome message
-    q.page['header'] = ui.header_card(
-        box='1 1 8 1',
-        title='HealthGuardian: Disease Risk Detector',
-        subtitle='Welcome!',
-    )
-
-    # Cards for diseases
-    q.page['Lung_Cancer'] = ui.tall_info_card(
-        box='1 3 2 4',
-        name='Lung_Cancer',
-        title='Lung Cancer',
-        caption='You can assess your risks from your medical records and habits.',
-        category='Risk Prediction',
-        image='https://images.pexels.com/photos/3225517/pexels-photo-3225517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    )
-
-    q.page['CVD'] = ui.tall_info_card(
-        box='3 3 2 4',
-        name='CVD',
-        title='Cardiovascular Disease',
-        caption='You can assess your risks from your medical records and habits.',
-        category='Risk Prediction',
-        image='https://images.pexels.com/photos/3225517/pexels-photo-3225517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    )
-
-    q.page['Diabetes'] = ui.tall_info_card(
-        box='5 3 2 4',
-        name='Diabetes',
-        title='Diabetes',
-        caption='You can assess your risks from your medical records and habits.',
-        category='Risk Prediction',
-        image='https://images.pexels.com/photos/3225517/pexels-photo-3225517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    )
-
-    q.page['example'] = ui.form_card(box='7 3 2 4', items=[
-            ui.text_l(content='Risk Assesment Form'),
-            ui.choice_group(name='choice_group', label='Pick the disease for risk assesment', value='B', required=True, choices=choices_pick),
-            # ui.button(name='show_inputs', label='Submit', primary=True),
-        ])
-
-    # if q.args.show_inputs:
-    del q.page['Diabetes']
-    q.page['example'] = ui.form_card(box='1 3 4 7', items=[
-            # ui.dropdown(name='dropdown', label='Pick one', value='B', required=True, choices=choices),
-            ui.textbox(name='textbox1', label='Input Number of times Pregnencies (if a male put zero)', value=q.args.input1),
-            ui.textbox(name='textbox2', label='Input Glocose Level', value=q.args.input2),
-            ui.textbox(name='textbox3', label='Input Blood Pressure', value=q.args.input3),
-            ui.textbox(name='textbox4', label='Input Skin Thickness', value=q.args.input4),
-            ui.textbox(name='textbox5', label='Input Insulin', value=q.args.input5),
-            ui.textbox(name='textbox6', label='Input BMI', value=q.args.input6),
-            ui.textbox(name='textbox7', label='Input Diabetes Pedigree Function Value', value=q.args.input7),
-            ui.textbox(name='textbox8', label='Input Age', value=q.args.input8),
-            ui.button(name='show_input', label='Submit', primary=True),
-        ])
-    
     if q.args.show_input:
-        del q.page['example']
-        del q.page['header']
-        del q.page['CVD']
-        del q.page['Diabetes']
-        del q.page['Lung_Cancer']
-        #  Store the values entered in text boxes in a list
-        input_values = [
-            q.args.textbox1,
-            q.args.textbox2,
-            q.args.textbox3,
-            q.args.textbox4,
-            q.args.textbox5,
-            q.args.textbox6,
-            q.args.textbox7,
-            q.args.textbox8,
-        ]
+            del q.page['example']
+            del q.page['header']
+            del q.page['CVD']
+            del q.page['Diabetes']
+            del q.page['Lung_Cancer']
+            #  Store the values entered in text boxes in a list
+            input_values = [
+                q.args.textbox1,
+                q.args.textbox2,
+                q.args.textbox3,
+                q.args.textbox4,
+                q.args.textbox5,
+                q.args.textbox6,
+                q.args.textbox7,
+                q.args.textbox8,
+            ]
 
-        # Get prediction using the predictor object
-        prediction = predictor.predict(input_values)#  Store the values entered in text boxes in a list
-        q.page['example2'] = ui.form_card(box='1 3 4 7', items=[
-            ui.text(f'textbox1={q.args.textbox}'),
-            ui.text_l(content=f'Prediction: {prediction}')  # Display prediction
-        ])
-        
+            # Get prediction using the predictor object
+            prediction = predictor.predict(input_values)#  Store the values entered in text boxes in a list
+            q.page['example2'] = ui.form_card(box='1 3 4 7', items=[
+                ui.text(f'textbox1={q.args.textbox}'),
+                ui.text_l(content=f'Prediction: {prediction}')  # Display prediction
+            ])
             
-    # else:
-    #     q.page['example'] = ui.form_card(box='7 3 2 4', items=[
-    #         ui.text_l(content='Risk Assesment Form'),
-    #         ui.choice_group(name='choice_group', label='Pick the disease for risk assesment', value='B', required=True, choices=choices_pick),
-    #         ui.button(name='show_inputs', label='Submit', primary=True),
-    #     ])
+            
+    else:
+        if q.args.show_inputs:
+            del q.page['Diabetes']
+            q.page['example'] = ui.form_card(box='1 3 4 7', items=[
+                    # ui.dropdown(name='dropdown', label='Pick one', value='B', required=True, choices=choices),
+                    ui.textbox(name='textbox1', label='Input Number of times Pregnencies (if a male put zero)', value=q.args.input1),
+                    ui.textbox(name='textbox2', label='Input Glocose Level', value=q.args.input2),
+                    ui.textbox(name='textbox3', label='Input Blood Pressure', value=q.args.input3),
+                    ui.textbox(name='textbox4', label='Input Skin Thickness', value=q.args.input4),
+                    ui.textbox(name='textbox5', label='Input Insulin', value=q.args.input5),
+                    ui.textbox(name='textbox6', label='Input BMI', value=q.args.input6),
+                    ui.textbox(name='textbox7', label='Input Diabetes Pedigree Function Value', value=q.args.input7),
+                    ui.textbox(name='textbox8', label='Input Age', value=q.args.input8),
+                    ui.button(name='show_input', label='Submit', primary=True),
+                ])
+        else: 
+            # Welcome message
+            q.page['header'] = ui.header_card(
+                box='1 1 8 1',
+                title='HealthGuardian: Disease Risk Detector',
+                subtitle='Welcome!',
+            )
+
+            # Cards for diseases
+            q.page['Lung_Cancer'] = ui.tall_info_card(
+                box='1 3 2 4',
+                name='Lung_Cancer',
+                title='Lung Cancer',
+                caption='You can assess your risks from your medical records and habits.',
+                category='Risk Prediction',
+                image='https://images.pexels.com/photos/3225517/pexels-photo-3225517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+            )
+
+            q.page['CVD'] = ui.tall_info_card(
+                box='3 3 2 4',
+                name='CVD',
+                title='Cardiovascular Disease',
+                caption='You can assess your risks from your medical records and habits.',
+                category='Risk Prediction',
+                image='https://images.pexels.com/photos/3225517/pexels-photo-3225517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+            )
+
+            q.page['Diabetes'] = ui.tall_info_card(
+                box='5 3 2 4',
+                name='Diabetes',
+                title='Diabetes',
+                caption='You can assess your risks from your medical records and habits.',
+                category='Risk Prediction',
+                image='https://images.pexels.com/photos/3225517/pexels-photo-3225517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+            
+            )
+            q.page['example'] = ui.form_card(box='7 3 2 4', items=[
+                ui.text_l(content='Risk Assesment Form'),
+                ui.choice_group(name='choice_group', label='Pick the disease for risk assesment', value='B', required=True, choices=choices_pick),
+                ui.button(name='show_inputs', label='Submit', primary=True),
+            ])
+
     await q.page.save()
+
+# Main page
+# @app('/health_guardian')
+# async def serve(q: Q):
+    
+#     if q.args.show_inputs:
+#         del q.page['Diabetes']
+#         q.page['example'] = ui.form_card(box='1 3 4 7', items=[
+#                 # ui.dropdown(name='dropdown', label='Pick one', value='B', required=True, choices=choices),
+#                 ui.textbox(name='textbox1', label='Input Number of times Pregnencies (if a male put zero)', value=q.args.input1),
+#                 ui.textbox(name='textbox2', label='Input Glocose Level', value=q.args.input2),
+#                 ui.textbox(name='textbox3', label='Input Blood Pressure', value=q.args.input3),
+#                 ui.textbox(name='textbox4', label='Input Skin Thickness', value=q.args.input4),
+#                 ui.textbox(name='textbox5', label='Input Insulin', value=q.args.input5),
+#                 ui.textbox(name='textbox6', label='Input BMI', value=q.args.input6),
+#                 ui.textbox(name='textbox7', label='Input Diabetes Pedigree Function Value', value=q.args.input7),
+#                 ui.textbox(name='textbox8', label='Input Age', value=q.args.input8),
+#                 ui.button(name='show_input', label='Submit', primary=True),
+#             ])
+        
+#         if q.args.show_input:
+#             del q.page['example']
+#             del q.page['header']
+#             del q.page['CVD']
+#             del q.page['Diabetes']
+#             del q.page['Lung_Cancer']
+#             #  Store the values entered in text boxes in a list
+#             input_values = [
+#                 q.args.textbox1,
+#                 q.args.textbox2,
+#                 q.args.textbox3,
+#                 q.args.textbox4,
+#                 q.args.textbox5,
+#                 q.args.textbox6,
+#                 q.args.textbox7,
+#                 q.args.textbox8,
+#             ]
+
+#             # Get prediction using the predictor object
+#             prediction = predictor.predict(input_values)#  Store the values entered in text boxes in a list
+#             q.page['example2'] = ui.form_card(box='1 3 4 7', items=[
+#                 ui.text(f'textbox1={q.args.textbox}'),
+#                 ui.text_l(content=f'Prediction: {prediction}')  # Display prediction
+#             ])
+            
+            
+#     else:
+#         # Welcome message
+#         q.page['header'] = ui.header_card(
+#             box='1 1 8 1',
+#             title='HealthGuardian: Disease Risk Detector',
+#             subtitle='Welcome!',
+#         )
+
+#         # Cards for diseases
+#         q.page['Lung_Cancer'] = ui.tall_info_card(
+#             box='1 3 2 4',
+#             name='Lung_Cancer',
+#             title='Lung Cancer',
+#             caption='You can assess your risks from your medical records and habits.',
+#             category='Risk Prediction',
+#             image='https://images.pexels.com/photos/3225517/pexels-photo-3225517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+#         )
+
+#         q.page['CVD'] = ui.tall_info_card(
+#             box='3 3 2 4',
+#             name='CVD',
+#             title='Cardiovascular Disease',
+#             caption='You can assess your risks from your medical records and habits.',
+#             category='Risk Prediction',
+#             image='https://images.pexels.com/photos/3225517/pexels-photo-3225517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+#         )
+
+#         q.page['Diabetes'] = ui.tall_info_card(
+#             box='5 3 2 4',
+#             name='Diabetes',
+#             title='Diabetes',
+#             caption='You can assess your risks from your medical records and habits.',
+#             category='Risk Prediction',
+#             image='https://images.pexels.com/photos/3225517/pexels-photo-3225517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+#         )
+
+#         q.page['example'] = ui.form_card(box='7 3 2 4', items=[
+#             ui.text_l(content='Risk Assesment Form'),
+#             ui.choice_group(name='choice_group', label='Pick the disease for risk assesment', value='B', required=True, choices=choices_pick),
+#             ui.button(name='show_inputs', label='Submit', primary=True),
+#         ])
+#     await q.page.save()
 
 # # Lung cancer page
 # @app('/lung_cancer')
